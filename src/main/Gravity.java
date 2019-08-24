76dcd335db3b35d8ef6b9a34226575293685d835
main <anvit.gupta_cs18@gla.ac.in>
2019-08-24 13:06:24 +0530

    assignmen 1 anvit gupta roll no:-13

--- src/main/Gravity.java
+++ src/main/Gravity.java
@@ -7,6 +7,8 @@ package main;
 // import java.util.Scanner;
 // uncomment the above line to use input in your program.
 
+import java.util.Scanner;
+
 public class Gravity {
 	// TODO 1: Create the main method
 	public static void main(String[] args) {
@@ -15,6 +17,16 @@ public class Gravity {
 		 *   b. Declare a constant for 'gravity', assume the value to be 9.8
 		 *   c. Calculate the result and print it to the screen.
 		 * */
+		Scanner sc = new Scanner((System.in));
+		double gravity=9.8;
+		double time= sc.nextDouble();
+		double speed;
+		double distance;
+		speed=gravity *time;
+		distance=(1/2.0)*gravity*time*time;
+		System.out.println("The speed of the object at time="+time+"Second  after its release is"+speed+"the distance the object has travelled in the time"+time+"seconds after the relase is"+distance);
+
+
 	}
 	// P.S: Refer to the README.md file for the problem statement, input & output.
 }
